2b9f0de2cc76f503eb566b179ffc7c4ab2864bd1
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-01-09 21:53:11 +0700

    add get all users and fix veriy token

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,15 +1,15 @@
-// import express
+// Import express
 const express = require("express");
 
-// import jwt
+// Import jwt
 const jwt = require("jsonwebtoken");
 
-const verifitoken = (req, res, next) => {
-  //get token
-  const token = req.headers["authorization"];
-  if (!token) return res.status(401).json({ message: "Unauthenticated" });
+const verifyToken = (req, res, next) => {
+  // Get token
+  const token = req.headers["authorization"].split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Unauthenticated." });
 
-  // verify token
+  // Verify token
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) return res.status(401).json({ message: "Invalid token" });
     req.userId = decoded.id;
@@ -17,4 +17,4 @@ const verifitoken = (req, res, next) => {
   });
 };
 
-module.exports = verifitoken;
+module.exports = verifyToken;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
-
 const router = express.Router();
-
 const registerController = require("../controllers/RegisterController");
 const loginController = require("../controllers/LoginController");
+const UserController = require("../controllers/UserController");
+const verifyToken = require("../middlewares/auth");
 
+// validates
 const { validateRegister, validateLogin } = require("../utils/validators/auth");
 
 // routes
 
 router.post("/register", validateRegister, registerController.register);
 router.post("/login", validateLogin, loginController.login);
+router.get("/admin/users", verifyToken, UserController.findUsers);
 
 module.exports = router;
