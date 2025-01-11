fa40428f74777cb1ea8b38314d6cc2d2f791f59a
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-01-11 22:10:57 +0700

    show list data users

--- frontend/src/views/admin/users/index.jsx
+++ frontend/src/views/admin/users/index.jsx
@@ -1,7 +1,38 @@
 import { Link } from "react-router-dom";
 import MainLayout from "../../../layout/MainLayout";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import api from "../../../services/api";
 
 export default function UsersIndex() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDataUsers = async () => {
+    const token = Cookies.get("token");
+
+    if (token) {
+      api.defaults.headers.common["Authorization"] = token;
+    }
+    try {
+      const response = await api.get("/admin/users");
+      setUsers(response.data.data);
+    } catch (error) {
+      console.error("There was an error fetching the users!", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDataUsers();
+
+    return () => {
+      setLoading(false);
+      setUsers([]);
+    };
+  }, []);
+
   return (
     <>
       <MainLayout>
@@ -17,7 +48,45 @@ export default function UsersIndex() {
           </div>
         </div>
         <hr />
-        <div className="bg-white p-2  h-[500px]">Halo users</div>
+        <div className="overflow-x-auto bg-white ">
+          <table className="table text-center">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Full Name</th>
+                <th>Email Address</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.length > 0 ? (
+                users.map((user, i) => (
+                  <tr key={i}>
+                    <td>{user?.name}</td>
+                    <td>{user?.email}</td>
+                    <td>
+                      <div className="flex items-center justify-center gap-2  ">
+                        <Link
+                          to={`/admin/users/${users.id}`}
+                          className="text-center bg-blue-700 text-white p-2 rounded-lg"
+                        >
+                          Edit
+                        </Link>
+                        <button className="text-center bg-red-700 text-white p-2 rounded-lg">
+                          Delete
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={3}>Tidak Ada Data</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </MainLayout>
     </>
   );
