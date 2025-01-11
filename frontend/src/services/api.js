09bbc22f5d990de45ace71419ea32846bad8ff77
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-01-11 22:40:58 +0700

    add auto redirect if token has expired

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,30 @@
 //import axios
 import axios from "axios";
-
+import Cookies from "js-cookie";
 const Api = axios.create({
   //set default endpoint API
   baseURL: "http://localhost:3000/api",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
+
+Api.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+
+  (error) => {
+    if (401 == error.response.status) {
+      alert("Sesi anda habis silakan login ulang!");
+      window.location.href = "/login";
+      Cookies.remove("token");
+      Cookies.remove("user");
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
 
 export default Api;
