335c5fe5973378ac9427daf10e21538337488118
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-01-11 12:11:55 +0700

    add showing current login user at dahboard page

--- frontend/src/views/admin/dashboard/index.jsx
+++ frontend/src/views/admin/dashboard/index.jsx
@@ -1,6 +1,17 @@
 import SidebarMenu from "../../../components/SidebarMenu";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
 
 export default function Dashboard() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const userData = Cookies.get("user");
+
+    if (userData) {
+      setUser(JSON.parse(userData));
+    }
+  }, []);
   return (
     <div className="flex flex-col  md:flex-row gap-4 items-center md:items-start pb-20">
       <div className="w-full px-4 md:w-4/12">
@@ -10,7 +21,11 @@ export default function Dashboard() {
         <h1 className="p-2 w-full bg-slate-600 rounded-t-lg text-white ">
           Dashboard
         </h1>
-        <div className="bg-white  h-[500px]"></div>
+        <div className="bg-white p-2  h-[500px]">
+          <p>
+            Selamat Datang <span className="font-bold">{user?.name}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
